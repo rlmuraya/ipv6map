0a152c792e2ebf20056780b5a20765175d73108b
Rebecca Muraya <rlmuraya@gmail.com>
2016-08-10 22:27:00 -0400

    Allow toggling active/inactive in VersionAdmin

--- ipv6map/geodata/admin.py
+++ ipv6map/geodata/admin.py
@@ -3,26 +3,32 @@ from django.contrib import admin
 from . import models
 
 
-class BaseReadOnlyAdmin(admin.ModelAdmin):
-    list_display_links = None
-
-    def has_change_permission(self, request, obj=None):
-        return False if obj else True
-
-
 @admin.register(models.Version)
-class VersionAdmin(BaseReadOnlyAdmin):
+class VersionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': ['publish_date', 'location_count'],
+        }),
+        ("Status", {
+            'fields': ['is_active'],
+        }),
+    ]
     list_display = ['publish_date', 'location_count', 'is_active']
     list_filter = ['is_active']
+    readonly_fields = ['publish_date', 'location_count']
 
     def location_count(self, obj):
         return obj.location_set.count()
 
 
 @admin.register(models.Location)
-class LocationAdmin(BaseReadOnlyAdmin):
+class LocationAdmin(admin.ModelAdmin):
     list_display = ['id', 'latitude', 'longitude', 'density', '_version']
+    list_display_links = None
     list_filter = ['version']
 
     def _version(self, obj):
         return obj.version.publish_date
+
+    def has_change_permission(self, request, obj=None):
+        return False if obj else True
