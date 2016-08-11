81669ab62ec026897430150cded13b20cb7a64c4
Rebecca Muraya <rlmuraya@gmail.com>
2016-08-10 23:01:17 -0400

    Map adjustments; user controls for heat radius

--- ipv6map/static/js/ipv6map.js
+++ ipv6map/static/js/ipv6map.js
@@ -1,29 +1,32 @@
 var map;
 
 $(function() {
+  /* Basic map configuration. */
   var initializeMap = function(container, center, zoom) {
-    /* Basic map configuration. */
-    map = L.map(container, {
+    map = L.map(container[0], {
+      center: center,
+      zoom: zoom,
       fullscreenControl: {
         position: "topright"
       }
     });
-    map.setView(center, zoom);
     map.addControl(L.tileLayer(
       'http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png',
       {
         attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, ' +
                      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         maxZoom: 19,
-        minZoom: 3,
+        minZoom: 2,
+        noWrap: true  // prevent duplicated worlds
       }
     ));
 
     /* Configure the heatmap layer. */
-    map.heat = L.heatLayer([], {});
+    map.heat = L.heatLayer([], {radius: 9});
     map.addControl(map.heat);
   }
 
+  /* Retrieve geojson data & display on the map. */
   var getGeodata = function(url) {
     $.getJSON(url).done(function(data) {
       /* Add the locations data to the map. */
@@ -46,7 +49,21 @@ $(function() {
     });
   }
 
+  /* Allow user to adjust heat map radius. */
+  $('#increase-radius').click(function(e) {
+    e.preventDefault();
+    map.heat.setOptions({radius: Math.min(map.heat.options.radius + 1, 25)});
+  });
+  $('#reset-radius').click(function(e) {
+    e.preventDefault();
+    map.heat.setOptions({radius: 9});
+  });
+  $('#decrease-radius').click(function(e) {
+    e.preventDefault();
+    map.heat.setOptions({radius: Math.max(map.heat.options.radius - 1, 1)});
+  });
+
   var container = $('#map');
-  initializeMap(container[0], [35.875189, -78.842686], 12);
+  initializeMap(container, [31.765537, -87.451171], 6);
   getGeodata(container.data('geodataUrl'));
 });
