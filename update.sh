fe4e35cef848df5a15cbd79fb2f275db869724a9
Rebecca Muraya <rlmuraya@gmail.com>
2016-08-10 21:27:51 -0400

    Reload server after updating

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+workon ipv6map
 git remote update
 git checkout master
 git reset --hard @{upstream}
 pip install -r requirements.txt
 npm install
 python manage.py migrate
-python manage.py collecstatic --noinput
+python manage.py collectstatic --noinput
+echo "Reloading server" && touch /var/www/rlmuraya_pythonanywhere_com_wsgi.py
