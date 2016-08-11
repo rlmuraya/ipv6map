39e1181fc5dd9e0134bf50b8b980d173f51fba94
Rebecca Muraya <rlmuraya@gmail.com>
2016-08-10 21:00:10 -0400

    Basic configuration of heatmap data

--- ipv6map/static/js/ipv6map.js
+++ ipv6map/static/js/ipv6map.js
@@ -19,13 +19,27 @@ $(function() {
       }
     ));
 
+    /* Configure the heatmap layer. */
+    map.heat = L.heatLayer([], {});
+    map.addControl(map.heat);
   }
 
   var getGeodata = function(url) {
     $.getJSON(url).done(function(data) {
-      // TODO
+      /* Add the locations data to the map. */
+      map.heat.setLatLngs(data.locations);
+
+      /* Display when this data was last updated. */
+      var version = new Date(data['version']);
+      $("#version").html("Data retrieved on " + version.toDateString() + ".<br>");
+
     }).fail(function(xhr, status, error) {
-      // TODO
+      /* Display error information in the info strip. */
+      var info = $("#info");
+      info.addClass('error');
+      info.find("p").html(
+        "An error occurred while retrieving the data:<br>" +
+        xhr.status + " - " + (xhr.responseJSON ? xhr.responseJSON.error : error));
     });
   }
 
